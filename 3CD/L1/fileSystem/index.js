00f8a7c44603e6a04a541603d95f0793e51c1048
govind <govindkumarjha2017@gmail.com>
2026-01-08 18:57:33 +0530

    8 jan 3 3CE

---
+++ 3CD/L1/fileSystem/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+// const loc = path.join(__dirname,'temp.txt')
+// // console.log(__dirname);
+// // console.log(loc);
+// const data = "this is temp string data";
+
+// fs.writeFile(loc,data,(err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log('file written succesfully!')
+//     }
+// })
+
+const loc = path.join(__dirname,'data','abc.txt')
+// const loc = path.join(__dirname,'data/abc.txt')
+
+fs.readFile(loc,{encoding:'utf-8'},(err,data)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        // console.log(data.toString());
+        console.log(data);
+    }
+})
