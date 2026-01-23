db75edb3f440f8f6198668f1b4d99b994a3a73cd
govind <govindkumarjha2017@gmail.com>
2026-01-23 10:09:32 +0530

    23 jan sec 3CD

---
+++ 3CD/L8/restFul/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const users = require('./data/users');
+
+app.set('view engine','ejs');
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req,res)=>{
+    res.send('Home page');
+})
+
+app.get('/users',(req,res)=>{
+    res.render('index',{users})
+})
+
+app.get('/user/new',(req,res)=>{
+    res.render('new');
+})
+
+app.post('/users',(req,res)=>{
+    let id;
+    if(users.length==0){
+        id=1
+    }
+    else{
+        id=users[users.length-1].id+1
+    }
+    const {name,age,password,city} = req.body;
+    users.push({
+        id,name,password,age,city
+    });
+    res.redirect('/users')
+})
+
+app.listen(5000,()=>{
+    console.log('server run at port 5000');
+})
