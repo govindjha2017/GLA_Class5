caf17f40a13a79b403533435680c2bfd38efb8fb
govind <govindkumarjha2017@gmail.com>
2026-01-23 13:47:11 +0530

    23 jan

---
+++ 3CC/L11/restful/data/users.js
@@ -0,0 +1,32 @@
+const users = [
+    {
+        id:1,
+        name:'rahul',
+        age:35,
+        city:'Delhi',
+        password:'rahul1233'
+    },
+    {
+        id:2,
+        name:'ajay',
+        age:25,
+        city:'Kolkata',
+        password:'ajay123'
+    },
+    {
+        id:3,
+        name:'teena',
+        age:25,
+        city:'Noida',
+        password:'teena123'
+    },
+    {
+        id:4,
+        name:'meena',
+        age:28,
+        city:'Mumbai',
+        password:'meena123'
+    },
+]
+
+module.exports = users;
