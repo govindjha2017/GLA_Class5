e8f5b597bfab645dd3d2ef9acb36a5d1ed7edb53
govind <govindkumarjha2017@gmail.com>
2026-02-06 15:04:27 +0530

    4 & 5 feb

---
+++ 3EA3EB/L1/index.js
@@ -0,0 +1,12 @@
+console.log(x);
+var x=40;
+
+fun();
+function fun(){
+    let a=10;
+    console.log(a);
+}
+// console.log(a);
+    console.log(p);
+let p = 30;
+console.log(p);
---
+++ 3EA3EB/L1/temp.js
@@ -0,0 +1,12 @@
+
+// for(let i=0;i<5;i++){
+//     setTimeout(()=>{
+//        console.log(i);
+//     },4000)
+// }
+
+for(var i=0;i<5;i++){
+    setTimeout(()=>{
+       console.log(i);
+    },4000)
+}
