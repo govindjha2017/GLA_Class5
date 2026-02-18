9ecc642b8a6d6f61c7404664722c42a6184bfe08
govind <govindkumarjha2017@gmail.com>
2026-02-18 14:50:42 +0530

    18 feb

---
+++ 3CD/L18/asyncAwait/script.js
@@ -0,0 +1,34 @@
+
+// async function fun(){
+//     return "Hello";
+// }
+
+// let x = fun();
+// console.log(x);
+
+
+function abc(){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve("kuchh bhi");
+        },4000)
+    })
+}
+
+// abc()
+//     .then((x)=>{
+//         console.log(x);
+//     })
+//     .catch((err)=>{
+//         console.log(err);
+//     })
+console.log("start");
+async function fetchData(){
+    console.log("inside function")
+    let x = await abc();
+    console.log("World");
+    console.log(x);
+}
+console.log("Hello")
+fetchData();
+console.log("End")
