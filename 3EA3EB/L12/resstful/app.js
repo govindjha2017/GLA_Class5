1e8a6ba29e9ad057b13f7783931adcdd4e32075a
govind <govindkummarjha2017@gmail.com>
2026-03-23 11:57:22 +0530

    23 march

---
+++ 3EA3EB/L12/resstful/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Product = require("./model/products")
+
+app.set("view engine","ejs");
+
+mongoose.connect("mongodb://localhost:27017/SSR-hons")
+    .then(()=>{console.log("DB conected")})
+    .catch(()=>{console.log("DB not conected")})
+
+
+app.get('/',(req,res)=>{
+    // res.send("Home page")
+    res.render("home")
+})
+
+app.get('/products',async (req,res)=>{
+    const products = await Product.find({})
+  
+    res.render("index",{products})
+})
+
+const PORT = 5000;
+app.listen(PORT,()=>{
+    console.log("Server run at port ",PORT)
+});
